ed1a9bf032261505072defa9c093f37568281d21
박민재 <48241766+minjae1025@users.noreply.github.com>
2019-05-28 17:01:24 +0900

    [Edit] 설치 후 창 한번 지움

--- install.sh
+++ install.sh
@@ -16,4 +16,5 @@ rm master.zip
 ./ChatterBotEnv/bin/python3 -m spacy download en
 mkdir extras
 mv ORIREADME.md dev-requirements.txt runtests.py tests LICENSE README.md docs graphics setup.cfg tests_django MANIFEST.in examples requirements.txt setup.py tox.ini extras
+clear
 echo "ChatterBot 폴더 내에서 'source ChatterBotEnv/bin/activate'를 실행하고 'python3 test.py' 를 실행하여 정상적으로 설치되었는지 확인하십시오."
