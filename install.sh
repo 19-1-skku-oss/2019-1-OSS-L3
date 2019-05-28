788f6547fc6667c9c2b4ba041bc3797dbfaaae0a
minjae1025 <cnsanet@gmail.com>
2019-05-28 22:12:59 +0900

    [ADD] chatterbot_corpus added

--- install.sh
+++ install.sh
@@ -4,18 +4,11 @@ echo "Hello!"
 
 sudo apt install -y git zip unzip python2.7 python3 python3-pip
 python3 -m pip install virtualenv
-mkdir ChatterBot
+git clone https://github.com/gunthercox/ChatterBot.git ChatterBot
 cd ChatterBot
 python3 -m virtualenv ChatterBotEnv
-wget https://github.com/19-1-skku-oss/2019-1-OSS-L3/archive/master.zip
-unzip master.zip
-mv 2019-1-OSS-L3-master/* .
-rmdir 2019-1-OSS-L3-master
-rm master.zip
 ./ChatterBotEnv/bin/pip3  install -r requirements.txt
 ./ChatterBotEnv/bin/pip3  install -r dev-requirements.txt
 ./ChatterBotEnv/bin/python3 -m spacy download en
-mkdir extras
-mv ORIREADME.md dev-requirements.txt runtests.py tests LICENSE README.md docs graphics setup.cfg tests_django MANIFEST.in examples install.sh requirements.txt setup.py tox.ini extras
 clear
 echo "ChatterBot 폴더 내에서 'source ChatterBotEnv/bin/activate'를 실행하고 'python3 test.py' 를 실행하여 정상적으로 설치되었는지 확인하십시오."
