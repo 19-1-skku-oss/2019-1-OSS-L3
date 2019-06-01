9bd978169e6de73ed923d6af8874f619aadb6e7a
박민재 <48241766+minjae1025@users.noreply.github.com>
2019-06-01 12:16:40 +0900

    [EDIT] 자신의 repo에서 쉽게 복사할 수 있도록 함

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Hello!"
+echo "먼저 https://github.com/19-1-skku-oss/2019-1-OSS-L3에 접속하여 자신의 repo에 저장소가 fork 되었는지 확인합니다"
+echo -e "fork 되어있다면 자신의 GitHub ID를 입력해주세요 : \c"
+
+read githubid
 
 sudo apt install -y git zip unzip python2.7 python3 python3-pip
 python3 -m pip install virtualenv
-git clone https://github.com/19-1-skku-oss/2019-1-OSS-L3.git ChatterBot
+git clone https://github.com/${githubid}/2019-1-OSS-L3.git ChatterBot
 cd ChatterBot
 python3 -m virtualenv ChatterBotEnv
 ./ChatterBotEnv/bin/pip3  install -r requirements.txt
