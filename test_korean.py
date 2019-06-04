d376b7f139aa35ac3e962e6204756aa29a4ab1a1
LEE SUNGJAE, rotmdwo skkusosc <rotmdehf@naver.com>
2019-06-04 23:01:32 +0900

    Added a Korean version tester

---
+++ test_korean.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot('Ron Obvious')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.korean")
+
+userinput = "안녕하세요"
+while not userinput == "exit":
+  print("chatterbot: ", end = "")
+  print(chatbot.get_response(userinput))
+  print("input (enter \"exit\" to exit): ",end = "")
+  userinput = input()
