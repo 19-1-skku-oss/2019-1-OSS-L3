b78bb3a07f12eb66b2eb3bff1829e9029bf874fd
minjae1025 <cnsanet@gmail.com>
2019-06-09 15:56:04 +0900

    [ADD] 대답을 선택할 때 위키피디아에서 받아오도록 하는 옵션 추가

--- chatterbot/response_selection.py
+++ chatterbot/response_selection.py
@@ -84,3 +84,56 @@ def get_random_response(input_statement, response_list, storage=None):
         len(response_list)
     ))
     return choice(response_list)
+
+
+def get_wikipedia_response(input_statement, response_list, storage=None):
+
+    first_data = get_first_response(input_statement, response_list, storage)
+    match_data = get_most_frequent_response(input_statement, response_list, storage)
+
+    if first_data.text == match_data.text:
+        return match_data
+
+    input_search_text = input_statement.search_text \
+                                if input_statement.search_text else \
+                                storage.tagger.get_text_index_string(input_statement.text)
+    
+    to_find = ''
+
+    for props in input_search_text.split(' '):
+        props_list = props.split(':')
+        if props_list[0] == "VERB":
+            to_find = props_list[1]
+            break
+
+    if not to_find:
+        return first_data
+
+    from requests import get
+
+    lang_code = 'en'
+    url = 'https://'+ lang_code + '.wikipedia.org/w/api.php'
+    query = 'wehaveto' + to_find +'&redirects=true'
+
+    response = get(url=(url + query)).json()
+    
+    pages = response['query']['pages']
+
+    if '-1' in pages.keys():
+        return first_data
+    else:
+        key = next(iter(pages.keys()))
+        respond = pages[key]['extract']
+
+        from .conversation import Statement
+
+        output_search_text = storage.tagger.get_text_index_string(respond)
+
+        rtn_data = Statement(
+            text=respond,
+            in_response_to=input_statement.text,
+            search_text=output_search_text,
+            search_in_respond_to=input_search_text
+        )
+
+        return rtn_data
